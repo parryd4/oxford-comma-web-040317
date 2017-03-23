85ddb6104c45dc1bad13115bb3af259ef27401f0
Daniel Parry <parryd424@gmail.com>
2017-03-23 10:40:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  size = array.size
+  case size
+  when 1
+    array.join
+  when 2
+    array.join(" and ")
+  else
+    "#{array[0,size-1].join(", ")}, and #{array.last}"
+  end
 end
